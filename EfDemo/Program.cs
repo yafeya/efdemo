dc005e5849d539dc2999bcc36311690d2474dd87
YANG,FEIYANG (K-China,ex1) <fei-yang.yang@keysight.com>
2018-11-05 16:02:11 +0800

    add example to execute stored procedure

--- EfDemo/Program.cs
+++ EfDemo/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -11,35 +12,109 @@ namespace EfDemo
     {
         static void Main(string[] args)
         {
-            var context = new PortalContext();
-            var products = context.Products.ToArray();
-            var users = context.Users.ToArray();
-            if (products == null || products.Count() == 0)
+            EagerLoad();
+            //ImplicitLoad();
+            //ExecuteProcedure();
+            Console.ReadKey();
+        }
+
+        private static void ExecuteProcedure()
+        {
+            var product = new Product { Name = "IOMonitor" };
+            using (var context = new PortalContext())
             {
-                var iolibs_product = new Product { Name = "IOLIBS" };
-                var user1 = new User { Name = "Keysight" };
-                var user2 = new User { Name = "Cisco" };
-                iolibs_product.Users = new[] { user1, user2 };
-                context.Products.Add(iolibs_product);
-                context.Users.AddRange(new[] { user1, user2 });
-                context.SaveChanges();
-            }
-            else
-            {
-                foreach (var product in products)
+                if (!context.Products.Any(x => x.Name == product.Name))
                 {
-                    Console.WriteLine($"Product: {product.Name}");
-                    var builder = new StringBuilder();
-                    foreach (var user in product.Users)
+                    var productNameParam = new SqlParameter("@name", SqlDbType.NVarChar, 250);
+                    productNameParam.Value = product.Name;
+                    var result = context.Database.ExecuteSqlCommand("insert_product @name", productNameParam);
+                }
+
+                var insert = context.Products.Where(x => x.Name == product.Name).FirstOrDefault();
+                if (insert != null)
+                {
+                    var productIdParam = new SqlParameter("@product_id", SqlDbType.Int, 4);
+                    productIdParam.Value = insert.ID;
+                    var user1 = new User { Name = "KCE", ProductID = insert.ID };
+                    var user2 = new User { Name = "IIO", ProductID = insert.ID };
+                    var userNameParam1 = new SqlParameter("@name", SqlDbType.NVarChar, 250);
+                    userNameParam1.Value = user1.Name;
+                    var userNameParam2 = new SqlParameter("@name", SqlDbType.NVarChar, 250);
+                    userNameParam2.Value = user2.Name;
+                    var user_sql = "insert_user @product_id, @name";
+                    context.Database.ExecuteSqlCommand(user_sql, productIdParam, userNameParam1);
+                    context.Database.ExecuteSqlCommand(user_sql, productIdParam, userNameParam2);
+                }
+            }
+        }
+
+        private static void ImplicitLoad()
+        {
+            using (var context = new PortalContext())
+            {
+                var products = context.Products.ToArray(); // To Use Implicit query, must close first connection.
+
+                if (products == null || products.Count() == 0)
+                {
+                    var iolibs_product = new Product { Name = "IOLIBS" };
+                    var user1 = new User { Name = "Keysight" };
+                    var user2 = new User { Name = "Cisco" };
+                    iolibs_product.Users = new[] { user1, user2 };
+                    context.Products.Add(iolibs_product);
+                    context.Users.AddRange(new[] { user1, user2 });
+                    context.SaveChanges();
+                }
+                else
+                {
+                    foreach (var product in products)
                     {
-                        builder.Append(user.Name).Append(" ");
+                        Console.WriteLine($"Product: {product.Name}");
+                        var builder = new StringBuilder();
+                        var users = context.Entry(product).Collection(p => p.Users).Query().Select(x => x).ToArray();
+                        foreach (var user in users)
+                        {
+                            builder.Append(user.Name).Append(" ");
+                        }
+                        Console.WriteLine($"Users: {builder.ToString()}");
+                        Console.WriteLine();
                     }
-                    Console.WriteLine($"Users: {builder.ToString()}");
-                    Console.WriteLine();
+                }
+            }
+        }
+
+        private static void EagerLoad()
+        {
+            using (var context = new PortalContext())
+            {
+                var products = context.Products.Include("Users"); //Eager loading.
+
+                if (products == null || products.Count() == 0)
+                {
+                    var iolibs_product = new Product { Name = "IOLIBS" };
+                    var user1 = new User { Name = "Keysight" };
+                    var user2 = new User { Name = "Cisco" };
+                    iolibs_product.Users = new[] { user1, user2 };
+                    context.Products.Add(iolibs_product);
+                    context.Users.AddRange(new[] { user1, user2 });
+                    context.SaveChanges();
+                }
+                else
+                {
+                    foreach (var product in products)
+                    {
+                        Console.WriteLine($"Product: {product.Name}");
+                        var builder = new StringBuilder();
+                        foreach (var user in product.Users)
+                        {
+                            builder.Append(user.Name).Append(" ");
+                        }
+                        Console.WriteLine($"Users: {builder.ToString()}");
+                        Console.WriteLine();
+                    }
                 }
             }
 
-            Console.ReadKey();
         }
+
     }
 }
